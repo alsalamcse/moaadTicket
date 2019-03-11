4cd30b6fc64d9aa7b8cb356ee37c463652a233c6
moaad <rakaz.hammad12@gmail.com>
2019-03-11 13:17:33 +0200

    main

--- app/src/main/java/com/moaadticket/moaadticket/StadiumActivity.java
+++ app/src/main/java/com/moaadticket/moaadticket/StadiumActivity.java
@@ -2,15 +2,140 @@ package com.moaadticket.moaadticket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class StadiumActivity extends AppCompatActivity {
-    private ListView LvStadium;
+    private Button btnR1;
+    private Button btnR2;
+    private Button btnR3;
+    private Button btnR4;
+    private Button btnR5;
+    private Button btnR6;
+    private Button btnR7;
+    private Button btnR8;
+    private Button btnR9;
+    private Button btnR10;
+    private Button btnR11;
+    private Button btnR12;
+    private Button btnR13;
+    private Button btnR14;
+    private Button btnR15;
+    private Button btnR16;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stadium);
-        LvStadium = (ListView)findViewById(R.id.LvStadium);
+        btnR1 = (Button) findViewById(R.id.btnR1);
+        btnR2 = (Button) findViewById(R.id.btnR2);
+        btnR3 = (Button) findViewById(R.id.btnR3);
+        btnR4 = (Button) findViewById(R.id.btnR4);
+        btnR5 = (Button) findViewById(R.id.btnR5);
+        btnR6 = (Button) findViewById(R.id.btnR6);
+        btnR7 = (Button) findViewById(R.id.btnR7);
+        btnR8 = (Button) findViewById(R.id.btnR8);
+        btnR9 = (Button) findViewById(R.id.btnR9);
+        btnR10 = (Button) findViewById(R.id.btnR10);
+        btnR11 = (Button) findViewById(R.id.btnR11);
+        btnR12 = (Button) findViewById(R.id.btnR12);
+        btnR13 = (Button) findViewById(R.id.btnR13);
+        btnR14 = (Button) findViewById(R.id.btnR14);
+        btnR15 = (Button) findViewById(R.id.btnR15);
+        btnR16 = (Button) findViewById(R.id.btnR16);
+        btnR1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnR1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnR2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });btnR3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }); btnR16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
