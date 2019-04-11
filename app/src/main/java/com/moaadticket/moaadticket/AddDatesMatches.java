f241fb36b686ba87ebec49537f5650bb51df689d
moaad <rakaz.hammad12@gmail.com>
2019-04-11 09:27:43 +0300

    spinner 123

--- app/src/main/java/com/moaadticket/moaadticket/AddDatesMatches.java
+++ app/src/main/java/com/moaadticket/moaadticket/AddDatesMatches.java
@@ -1,13 +1,9 @@
 package com.moaadticket.moaadticket;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class AddDatesMatches extends AppCompatActivity {
-    private EditText edtDate ,edtTime ,edtMatch;
-    private TextView tvDate ,tvTime, tvMatch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
